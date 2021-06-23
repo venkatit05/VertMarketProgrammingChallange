14118edd6572213a1d1635a71488c502b4484da0
venkatit05 <86369976+venkatit05@users.noreply.github.com>
2021-06-23 09:01:24 -0700

    Add files via upload

---
+++ ConsoleApp1/Magazine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Magazine
+    {
+       public string Id { get; set; }
+       public string Category { get; set; }
+
+       public string Name { get; set; }
+    }
+
+    class Subscriber
+    {
+        public Subscriber()
+        {
+            magIds = new List<string>();
+        }
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<string> magIds { get; set; }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,11 @@
++++++++++++++++++++++++++++++
+TestAgni - Console Application...
+
+Follow the below step to run the TestAgni console application..
+
+1. Unzip the TestAgni.zip file 
+2. Open the TestAgni.sln 
+3. Just run the application
+	- It will generate the token automatically and calls the subsequent calls and pass the token
+	- Get the result and post the answer to http://magazinestore.azurewebsites.net/api/answer with token.
+
